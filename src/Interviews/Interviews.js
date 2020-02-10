617f7a8629a79c10189b29509f77a949bd6e7b92
SanthoshVelaga <santhosh@santhoshs-mbp.lan>
2020-02-10 01:30:30 -0800

    edited the interviews page

--- src/Interviews/Interviews.js
+++ src/Interviews/Interviews.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import Samplequestions from '../Templates/Questions/Samplequestions/Samplequestions'
 
 const Interviews = () => {
     return (<>
     <h1>Interviews</h1>
-        <p>Would you hire this employee? Why?</p>
-        <input type="text" name="name" style={{width:'35%', height: '75px'}} />
-        <p>Did this employee have the right qualifications?</p>
-        <input type="text" name="name" style={{width:'35%', height: '75px'}} />
-        <p>Would they be a good addition to our culutre?</p>
-        <input type="text" name="name" style={{width:'35%', height: '75px'}} />
-        <p>Any red flags or areas I should dig a little deeper in?</p>
-        <input type="text" name="name" style={{width:'35%', height: '75px'}} />
-        <p>Anything else you'd like to share?</p>
-        <input type="text" name="name" style={{width:'35%', height: '75px', marginBottom: '50px'}} />
-        <br></br>
-        <button style={{margin: 'auto', marginBottom: '50px'}}>Submit</button>
+        <Samplequestions />
         </>);
 }
  
